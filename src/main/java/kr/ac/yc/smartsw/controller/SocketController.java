8828a17e8b7f820ddc0a41a40198165871f1a267
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2022-06-17 13:00:07 +0900

    소켓통신 부분 추가
    socket/IP/msg 경로를 사용하면
    IP에 msg를 소켓통신으로 보내줌.

--- src/main/java/kr/ac/yc/smartsw/controller/SocketController.java
+++ src/main/java/kr/ac/yc/smartsw/controller/SocketController.java
@@ -1,10 +1,17 @@
 package kr.ac.yc.smartsw.controller;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.ac.yc.smartsw.service.RESTVO;
 import kr.ac.yc.smartsw.service.SocketService;
 
 @Controller
@@ -15,9 +22,11 @@ public class SocketController {
 	@Autowired
 	SocketService service;
 	
-	@GetMapping
-	public String viewPage() {
-		service.sendMsg("test");
-		return "socket";
+	@GetMapping("/{serverIP}/{msg}")
+	@ResponseBody
+	public String sendMsg(@PathVariable Optional<String> serverIP, @PathVariable Optional<String> msg) {
+		SocketService.SERVER_IP = serverIP.orElse("localhost");
+		service.sendMsg(msg.orElse("test"));
+		return msg.orElse("test");
 	}
 }
